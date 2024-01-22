b746283d8036e450925912ad9024c87fab526888
codabat <lorenzo.vannucci@gmail.com>
2024-01-22 16:14:35 +0100

    Inserimento di un Header con il menu - creato un componente riutilizzabile utilizzando Nx che ha creato le folders: libs/ui... Poi configurato in index.ts per importare con "@ebuy/ui": ["libs/ui/src/index.ts"] in tsconfig.base.json

--- libs/ui/src/lib/header.tsx
+++ libs/ui/src/lib/header.tsx
@@ -1,15 +1,52 @@
-import styles from './header.module.css';
 import { Menu, Container, Icon, Label } from 'semantic-ui-react';
 
-/* eslint-disable-next-line */
-export interface HeaderProps {}
-
-export function Header(props: HeaderProps) {
+export function Header() {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Header!</h1>
-    </div>
+    <Menu fixed="top" inverted>
+      <Container fixed="top">
+        <Menu.Item as="a" header>
+          eBuy.com
+        </Menu.Item> 
+        <MenuItems />
+        <Menu.Item position='right'>
+          <Label>
+            <Icon name="shopping cart">
+              00
+            </Icon>
+          </Label>
+        </Menu.Item>
+      </Container>
+    </Menu>
   );
 }
+
+const MenuItems = () => {
+  return (
+    <>
+      {NAV_ITEMS.map((navItem) =>(
+        <Menu.Item key={navItem.label}>
+          <a href={navItem.href ?? '#'}>{navItem.label}</a>
+        </Menu.Item>
+      ))}
+    </>
+  );
+};
+
+  interface NavItem {
+    label: string;
+    href?: string;
+  }
+  
+  const NAV_ITEMS: Array<NavItem> = [
+    {
+      label: 'Catalog',
+      href: '/',
+    },
+    {
+      label: 'Checkout',
+      href: '/checkout',
+    },
+  ];
+
 
 export default Header;
