68f5f8dc8163e60fc4cf3adf10e0aa9c3c9db9fc
codabat <lorenzo.vannucci@gmail.com>
2024-02-22 22:49:49 +0100

    Pulizia merge

--- apps/app-shell/src/app/app.tsx
+++ apps/app-shell/src/app/app.tsx
@@ -4,12 +4,8 @@ import {Container} from 'semantic-ui-react';
 import 'semantic-ui-ccs/semantic.min.css';
 import {Header} from '@ebuy/ui';
 
-<<<<<<< HEAD
 const Catalog = React.lazy(() => import ('catalog/Module'));
 const Checkout = React.lazy(() => import ('checkout/Module'));
-=======
-const Catalog = React.lazy(() => import('catalog/Module'));
->>>>>>> 6b1557d0aaa926f004dc26c2cbddd0fa20b431a7
 
 export function App() {
   return (
@@ -17,12 +13,8 @@ export function App() {
       <Container style={{marginTop: '5rem'}}>
         <Header />
         <Routes>
-<<<<<<< HEAD
           <Route path='/' element={<Catalog />}/>
           <Route path="/checkout" element={<Checkout />}/>
-=======
-          <Route path="/" element={<Catalog />}/>
->>>>>>> 6b1557d0aaa926f004dc26c2cbddd0fa20b431a7
         </Routes>
       </Container>
     </React.Suspense>
